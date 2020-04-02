68704bc0a3fddd2316af245615303af857c6a501
Lucas <biblion84@users.noreply.github.com>
2020-04-02 16:49:43 +0200

    Modal now work, can only place marker after click on signalement button

--- Bootstrap/assets/js/map.js
+++ Bootstrap/assets/js/map.js
@@ -11,12 +11,38 @@ function addMarker(location){
 
 function onMapClick(e){
     addMarker(e)
+    map.removeEventListener("click", onMapClick)
 }
 
 let policeIcon = L.icon({
     iconUrl : "assets/icon/police_icon.png",
     iconAnchor:   [50, 50],
 });
+
+let policeButton = document.getElementById("policeButton")
+
+policeButton.onclick = () => {
+    Swal.fire({
+        title: 'Position du contrôle',
+        text: "Voulez vous signaler un contrôle de police ?",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Oui',
+        showCloseButton : true,
+        cancelButtonText: "Non"
+
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Confirmation',
+                "Veuillez cliquer sur l'endroit où se situe le contrôle",
+                'info'
+            )
+            map.on("click",onMapClick)
+        }
+    })
+}
 
 
 var map = L.map('mapid').fitWorld();
@@ -31,8 +57,29 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 map.locate({setView: true, maxZoom: 16});
 map.on('locationfound', onLocationFound);
+// var stateChangingButton = L.easyButton({
+//     position:  'topRight',
+//     states: [{
+//         stateName: 'zoom-to-forest',        // name the state
+//         icon:      '<img src="assets/icon/policeButton.png">',               // and define its properties
+//         title:     'zoom to a forest',      // like its title,,
+//         position: "bottomLeft",
+//         onClick: function(btn, map) {       // and its callback
+//             map.setView([46.25,-121.8],10);
+//             btn.state('zoom-to-school');    // change state on click!
+//         }
+//     }, {
+//         stateName: 'zoom-to-school',
+//         icon:      'fa-university',
+//         title:     'zoom to a school',
+//         onClick: function(btn, map) {
+//             map.setView([42.3748204,-71.1161913],16);
+//             btn.state('zoom-to-forest');
+//         }
+//     }]
+// });
+
+// stateChangingButton.addTo( map );
 
 
-
-
-map.on("click",onMapClick)
+// map.on("click",onMapClick)
