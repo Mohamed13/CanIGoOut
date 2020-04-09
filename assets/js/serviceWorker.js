adf768ed1e2391975c9f5e4accbaaa5754c47b81
Lucas <biblion84@users.noreply.github.com>
2020-04-09 15:08:32 +0200

    FIX : service worker scope again, don't mind me

--- assets/js/serviceWorker.js
+++ assets/js/serviceWorker.js
@@ -49,17 +49,3 @@ self.addEventListener("refreshOffline", function () {
     });
 });
 
-if ("serviceWorker" in navigator) {
-    if (navigator.serviceWorker.controller) {
-        console.log("[PWA Builder] active service worker found, no need to register");
-    } else {
-        // Register the service worker
-        navigator.serviceWorker
-            .register("assets/js/serviceWorker.js", {
-                scope: "/CanIGoOut/"
-            })
-            .then(function (reg) {
-                console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-            });
-    }
-}
