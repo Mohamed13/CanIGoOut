78b41d9c1218923ece939477b6bba45bf5dc1a2a
Mohamed Badaoui <mohamed.badaoui13@gmail.com>
2020-04-09 10:01:12 +0200

    [PWA] - Add leaflet + map

--- progressive/src/app/maps/maps.component.ts
+++ progressive/src/app/maps/maps.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.sass']
+  styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
 
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
+  }
 }
