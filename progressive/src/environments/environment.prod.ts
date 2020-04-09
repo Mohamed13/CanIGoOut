6be4d15b3c3cb31b4d57cf0bb3cac46c4838f860
Mohamed Badaoui <mohamed.badaoui13@gmail.com>
2020-04-09 09:20:20 +0200

    [PWA] - remove firebase key

--- progressive/src/environments/environment.prod.ts
+++ progressive/src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBdKO4PvQldfP-5VLW9qqbS6sbUvwpwL8M",
-    authDomain: "canigoout-25b12.firebaseapp.com",
-    databaseURL: "https://canigoout-25b12.firebaseio.com",
-    projectId: "canigoout-25b12",
-    storageBucket: "canigoout-25b12.appspot.com",
-    messagingSenderId: "433104589309",
-    appId: "1:433104589309:web:d8ebcf3a82902ff503c994",
-    measurementId: "G-SH80DS4GD5"
-  }
 };
