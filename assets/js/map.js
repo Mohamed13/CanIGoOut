200b0e77119b3ca4a5f8cf54e2d6c84a66a07a31
Lucas <biblion84@users.noreply.github.com>
2020-04-09 14:05:21 +0200

    Fix view on smartphone + block zoom to better reflect radius in which we show controls

--- assets/js/map.js
+++ assets/js/map.js
@@ -127,6 +127,7 @@ policeButton.onclick = () => {
 var map = L.map('mapid').fitWorld();
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWRvZiIsImEiOiJjazd5a3FxbHMwNmxhM2twbWlybWZma3g1In0.yYqxxmkQk9YrP3GVTrQsHA', {
     maxZoom: 18,
+    minZoom: 12,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
