f4c587f1ffc95b83c423aaeeb15845cb822bd212
Mohamed Badaoui <mohamed.badaoui13@gmail.com>
2020-04-17 12:11:05 +0200

    [PWA] - changement du manifest

--- progressive/src/app/app.component.spec.ts
+++ progressive/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'progressive'`, () => {
+  it(`should have as title 'canigoout'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('progressive');
+    expect(app.title).toEqual('canigoout');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to progressive!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to canigoout!');
   });
 });
